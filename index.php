50a35a96d6c67380e9b44f3692791053583bc2e1
Avigit Mandal <avigitmandal@Avigits-MacBook-Air.local>
2024-07-18 10:08:33 +0545

    condition for access_token

--- index.php
+++ index.php
@@ -8,5 +8,10 @@ $client->setAuthConfig('credentials.json');
 $client->setRedirectUri('http://localhost/calendar-event/oauth2callback.php');
 $client->addScope(Google_Service_Calendar::CALENDAR);
 
-
+if (!isset($_SESSION['access_token']) || $_SESSION['access_token']) {
+    $authUrl = $client->createAuthUrl();
+    echo "<a href='$authUrl'>Connect to Google Calendar</a>";
+} else {
+    header('Location: list_events.php');
+}
 ?>
